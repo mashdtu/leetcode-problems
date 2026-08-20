c704d4e0d33a8e31654be39fca2a920be02a90a4
mashdtu <mashdtu@pm.me>
2026-08-21 01:35:45 +0200

    reorganizing subfolders

--- 125-valid-palindrome/Solution.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        return s.equals(new StringBuilder(s).reverse().toString());
-    }
-}
