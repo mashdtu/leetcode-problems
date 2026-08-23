9790d97baacfeca8d7fef1a490101dda4063d62a
mashdtu <mashdtu@pm.me>
2026-08-23 04:15:08 +0200

    solution for problem 9

--- 0009-palindrome-number/Solution.java
+++ 0009-palindrome-number/Solution.java
@@ -1,7 +1,9 @@
 class Solution {
-    public boolean isPalindrome (int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) { return false; }
-        
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
         int reverse = 0;
         while (x > reverse) {
             reverse = reverse * 10 + x % 10;
