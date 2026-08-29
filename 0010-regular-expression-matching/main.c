2933457032faa2eebb5953d2084926ce41134239
mashdtu <mashdtu@pm.me>
2026-08-30 00:06:16 +0200

    wrote head function to convert regex to NFA, missing helper functions for it

--- 0010-regular-expression-matching/main.c
+++ 0010-regular-expression-matching/main.c
@@ -42,10 +42,47 @@ bool isActiveState(uint64_t activeStates, uint8_t s)
     return (activeStates & (UINT64_C(1) << s)) != 0;
 }
 
+NFA emptyNFA(void)
+{
+}
+
+NFA addLiteral(char literal)
+{
+}
+NFA addWildcard(void)
+{
+}
+NFA addKleeneClosure(NFA fragment)
+{
+}
+NFA concatenate(NFA left, NFA right)
+{
+}
+
+NFA regexToNFA(char *p)
+{
+    NFA n = emptyNFA();
+    for (size_t i = 0; p[i] != '\0'; i++)
+    {
+        NFA fragment = (p[i] == '.') ? addWildcard()
+                                     : addLiteral(p[i]);
+
+        if (p[i + 1] == '*')
+        {
+            fragment = addKleeneClosure(fragment);
+            i++;
+        }
+
+        n = concatenate(n, fragment);
+    }
+
+    return n;
+}
+
 bool isMatch(char *s, char *p)
 {
     uint64_t activeStates = 0;
-    
+    NFA nfa = regexToNFA(p);
 }
 
 int main(int argc, char const *argv[])
